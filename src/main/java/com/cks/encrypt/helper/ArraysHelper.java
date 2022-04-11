c089295e55d2d6f91ca0f3a265406f2af4af70a2
Colin Saldanha <colin.saldanha@gmail.com>
2022-04-10 20:10:29 -0400

    Change to static for copyBytes helper method

--- src/main/java/com/cks/encrypt/helper/ArraysHelper.java
+++ src/main/java/com/cks/encrypt/helper/ArraysHelper.java
@@ -11,7 +11,7 @@ package com.cks.encrypt.helper;
  */
 public class ArraysHelper {
 
-    private void copyBytes(byte[] dst, int start, byte[] src, int offset, int len) {
+    public static void copyBytes(byte[] dst, int start, byte[] src, int offset, int len) {
         if ((dst.length - start) < len)
             throw new IllegalArgumentException("Source bytes length are too long for destination");
 
