520db6511b1bd31321c5616e045de5c1cbaf9c5f
Colin Saldanha <colin.saldanha@gmail.com>
2022-08-28 13:12:52 -0400

    Updates to AESCommand unit test class

--- src/test/java/com/cks/encrypt/cli/AESCommandTest.java
+++ src/test/java/com/cks/encrypt/cli/AESCommandTest.java
@@ -7,6 +7,8 @@ package com.cks.encrypt.cli;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -19,19 +21,35 @@ public class AESCommandTest {
     @Test
     public void testExecute_whenValidFileNameFlag() throws Exception {
         Command command = new AESCommand();
-        command.addFlag(new Flag("filename", "aes.key"));
+        List<String> fileNames = new ArrayList<>();
+        fileNames.add("aes.key");
+        Flag filesFlag = new Flag("files", fileNames);
+        command.addFlag(filesFlag);
         command.execute();
         assertNotNull(Paths.get("aes.key"));
         Files.deleteIfExists(Paths.get("aes.key"));
     }
 
     @Test
-    public void testExecute_whenNoFileNameFlag() {
-        try {
-            Command command = new AESCommand();
-            command.execute();
-            fail("Exception not thrown");
-        } catch (Exception x) {
-        }
+    public void testExecute_whenMultipleFilesSpecified() throws Exception {
+        Command command = new AESCommand();
+        List<String> fileNames = new ArrayList<>();
+        fileNames.add("file1");
+        fileNames.add("file2");
+        Flag filesFlag = new Flag("files", fileNames);
+        command.addFlag(filesFlag);
+        command.execute();
+        assertNotNull(Paths.get("file1"));
+        assertNotNull(Paths.get("file2"));
+        Files.deleteIfExists(Paths.get("file1"));
+        Files.deleteIfExists(Paths.get("file2"));
+    }
+
+    @Test
+    public void testExecute_whenNoFileNameFlag() throws Exception {
+        Command command = new AESCommand();
+        command.execute();
+        assertNotNull(Paths.get("aes.key"));
+        Files.deleteIfExists(Paths.get("aes.key"));
     }
 }
