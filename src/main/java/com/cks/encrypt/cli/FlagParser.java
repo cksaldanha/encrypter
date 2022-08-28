e3841149aa026b4439e5c232da2476eb43b88c48
Colin Saldanha <colin.saldanha@gmail.com>
2022-08-27 22:46:44 -0400

    Add separate parsing methods to FlagParser
    These separate methods provide an easier way to extract the different
    flag formats of a command string

--- src/main/java/com/cks/encrypt/cli/FlagParser.java
+++ src/main/java/com/cks/encrypt/cli/FlagParser.java
@@ -5,7 +5,9 @@
  */
 package com.cks.encrypt.cli;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import java.util.logging.Logger;
 import java.util.regex.Matcher;
@@ -15,11 +17,12 @@ import java.util.regex.Pattern;
  *
  * @author colin.saldanha
  */
-public abstract class FlagParser {
+public class FlagParser {
 
     private static final Logger LOGGER = Logger.getLogger("com.cks");
-    private static final Pattern KEY_VALUE_PATTERN = Pattern.compile("--\\w+=[\\w\\.]+");
-    private static final Pattern KEY_ONLY_PATTERN = Pattern.compile("--\\w+\\s*?");
+    private static final Pattern KEY_VALUE_PATTERN = Pattern.compile("--(?<key>[\\w-]+)(?==(?<value>[\\w-\\.]+\\s*))");
+    private static final Pattern KEY_ONLY_PATTERN = Pattern.compile("--(?<key>[\\w-]+)(?=\\s*)");
+    private static final Pattern NO_KEY_PATTERN = Pattern.compile("(?<=\\s)[\\w]");
 
     public Set<Flag> parseFlags(String line) {
         Set<Flag> flags = new HashSet<>();
@@ -34,6 +37,37 @@ public abstract class FlagParser {
             }
         }
         return flags;
+    }
+
+    public Set<Flag> parseKeyValueFlags(String line) {
+        Matcher matcher = KEY_VALUE_PATTERN.matcher(line);
+        Set<Flag> flags = new HashSet<>();
+        while (matcher.find()) {
+            String key = matcher.group("key");
+            String value = matcher.group("value");
+            flags.add(new Flag(key, value));
+        }
+        return flags;
+    }
+
+    public Set<Flag> parseKeyOnlyFlags(String line) {
+        Matcher matcher = KEY_ONLY_PATTERN.matcher(line);
+        Set<Flag> flags = new HashSet<>();
+        while (matcher.find()) {
+            String key = matcher.group("key");
+            flags.add(new Flag(key, null));
+        }
+        return flags;
+    }
+
+    public Flag parseFileNamesFlag(String line) {
+        Matcher matcher = NO_KEY_PATTERN.matcher(line);
+        List<String> fileNames = new ArrayList<>();
+        while (matcher.find()) {
+            String fileName = matcher.group();
+            fileNames.add(fileName);
+        }
+        return fileNames.size() > 0 ? new Flag("files", fileNames) : null;
     }
 
     public Set<Flag> parseFlagSet(Pattern pattern, String line) {
