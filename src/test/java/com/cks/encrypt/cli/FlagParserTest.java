48ba6abdb68bff6f0882b85fef3c9e3072994f7c
Colin Saldanha <colin.saldanha@gmail.com>
2022-08-28 08:46:50 -0400

    Add additional unit tests for FlagParser

--- src/test/java/com/cks/encrypt/cli/FlagParserTest.java
+++ src/test/java/com/cks/encrypt/cli/FlagParserTest.java
@@ -18,6 +18,66 @@ import static org.junit.Assert.*;
 public class FlagParserTest {
 
     @Test
+    public void testParseKeyValueFlags_singleKeyValueFlag() {
+        FlagParser parser = new FlagParser();
+        Set<Flag> actual = parser.parseKeyValueFlags("aes --first=first.Values");
+        Set<Flag> expected = new HashSet<>();
+        expected.add(new Flag("first", "first.Values"));
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testParseKeyValueFlags_multipleKeyValueFlags() {
+        FlagParser parser = new FlagParser();
+        Set<Flag> actual = parser.parseKeyValueFlags("aes --first=first.Values --second=secondValue");
+        Set<Flag> expected = new HashSet<>();
+        expected.add(new Flag("first", "first.Values"));
+        expected.add(new Flag("second", "secondValue"));
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testParseKeyValueFlags_multipleKeyValueFlagsWithSpaces() {
+        FlagParser parser = new FlagParser();
+        Set<Flag> actual = parser.parseKeyValueFlags("aes  --first=first.Values   --second=secondValue  ");
+        Set<Flag> expected = new HashSet<>();
+        expected.add(new Flag("first", "first.Values"));
+        expected.add(new Flag("second", "secondValue"));
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testParseKeyOnlyFlags_singleKeyOnlyFlag() {
+        FlagParser parser = new FlagParser();
+        Set<Flag> actual = parser.parseKeyOnlyFlags("aes --first");
+        Set<Flag> expected = new HashSet<>();
+        expected.add(new Flag("first", null));
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testParseKeyOnlyFlags_multipleKeyOnlyFlags() {
+        FlagParser parser = new FlagParser();
+        Set<Flag> actual = parser.parseKeyOnlyFlags("aes --first --second --third");
+        Set<Flag> expected = new HashSet<>();
+        expected.add(new Flag("first", null));
+        expected.add(new Flag("second", null));
+        expected.add(new Flag("third", null));
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testParseKeyOnlyFlags_multipleKeyOnlyFlagsWithSpaces() {
+        FlagParser parser = new FlagParser();
+        Set<Flag> actual = parser.parseKeyOnlyFlags("aes  --first   --second  --third  ");
+        Set<Flag> expected = new HashSet<>();
+        expected.add(new Flag("first", null));
+        expected.add(new Flag("second", null));
+        expected.add(new Flag("third", null));
+        assertEquals(expected, actual);
+    }
+
+    @Test
     public void testParseFlags_singleKeyValueFlag() {
         FlagParser parser = new FlagParser();
         Set<Flag> actual = parser.parseFlags("aes --first=first.Values");
