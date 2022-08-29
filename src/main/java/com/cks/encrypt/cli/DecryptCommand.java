34444a3abc0ef449ebfeb1a87fc8d8b275fcbf86
Colin Saldanha <colin.saldanha@gmail.com>
2022-08-28 22:36:23 -0400

    Refactor EncryptCommand and DecryptCommand classes
    These classes will now extend the more general EncryptDecryptCommand
    class since they share many of the flags and validation functionality.

--- src/main/java/com/cks/encrypt/cli/DecryptCommand.java
+++ src/main/java/com/cks/encrypt/cli/DecryptCommand.java
@@ -5,46 +5,19 @@
  */
 package com.cks.encrypt.cli;
 
-import com.cks.encrypt.encryption.EncryptionAlgorithms;
-import java.util.HashSet;
-import java.util.Set;
-
 /**
  *
  * @author colin.saldanha
  */
-public class DecryptCommand extends Command {
-
-    private static final Set<String> VALID_FLAG_KEYS;
-
-    static {
-        VALID_FLAG_KEYS = new HashSet<>();
-        VALID_FLAG_KEYS.add("mode");
-        VALID_FLAG_KEYS.add("type");
-        VALID_FLAG_KEYS.add("files");
-    }
+public class DecryptCommand extends EncryptDecryptCommand {
 
     public DecryptCommand(Flag... flags) {
         super(flags);
-        if (getFlag("mode").equals(EncryptionAlgorithms.RSA)) {
-            if (!containsFlag("type") || !getFlag("type").getValue().isPresent()) {
-                throw new IllegalArgumentException("Must provide type argument with RSA encryption");
-            }
-            String type = (String) getFlag("type").getValue().get();
-            if (!type.equals("public") && !type.equals("private")) {
-                throw new IllegalArgumentException("Invalid type argument for RSA encryption");
-            }
-        }
     }
 
     @Override
     public String getCommand() {
         return Command.CMD_DECRYPT;
-    }
-
-    @Override
-    boolean validateFlag(Flag flag) {
-        return VALID_FLAG_KEYS.contains(flag.getKey());
     }
 
     @Override
