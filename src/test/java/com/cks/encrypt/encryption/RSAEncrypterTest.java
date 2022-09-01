2639c654bb623433f602f6afc99f1463780ad1b1
Colin Saldanha <colin.saldanha@gmail.com>
2022-08-31 23:16:05 -0400

    Refactor unit tests of RSAEncrypter class
    Certain tests were updated and others were added due to the revisions
    RSAEncrypter. Test titles were given more descriptive names, as well.

--- src/test/java/com/cks/encrypt/encryption/RSAEncrypterTest.java
+++ src/test/java/com/cks/encrypt/encryption/RSAEncrypterTest.java
@@ -6,18 +6,15 @@
 package com.cks.encrypt.encryption;
 
 import com.cks.encrypt.encryption.RSAEncrypter.KeyType;
-import java.lang.reflect.Field;
-import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.security.GeneralSecurityException;
 import java.security.Key;
-import java.security.SecureRandom;
+import org.junit.AfterClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import org.junit.Before;
-import org.mockito.Mockito;
+import org.junit.BeforeClass;
 import static org.mockito.Mockito.*;
 
 /**
@@ -29,11 +26,25 @@ public class RSAEncrypterTest {
     Key keyMock;
     
     byte[] keyStub;
-    
+
+    @BeforeClass
+    public static void GenerateKeyFiles() throws Exception {
+        RSAEncrypter encrypter = new RSAEncrypter();
+        encrypter.generateKeyPair();
+        encrypter.savePrivateKey(Paths.get("private.key"));
+        encrypter.savePublicKey(Paths.get("public.key"));
+    }
+
     @Before
     public void setup() {
         keyMock = mock(Key.class);
         keyStub = "sample key bytes".getBytes();
+    }
+
+    @AfterClass
+    public static void RemoveKeyFiles() throws Exception {
+        Files.deleteIfExists(Paths.get("private.key"));
+        Files.deleteIfExists(Paths.get("public.key"));
     }
 
     @Test
@@ -48,28 +59,57 @@ public class RSAEncrypterTest {
     }
 
     @Test
-    public void testEncrypt_Simple1() throws GeneralSecurityException {
-        Path keyFile = Paths.get("public.txt");
+    public void testEncrypt_withPublicKey() throws Exception {
         RSAEncrypter encrypter = new RSAEncrypter();
+
         String msg = "This is a test message";
         byte[] plainBytes = msg.getBytes();
-        byte[] cipherBytes = encrypter.encrypt(plainBytes, keyFile, KeyType.PUBLIC);
+        byte[] cipherBytes = encrypter.encrypt(plainBytes, Paths.get("public.key"), KeyType.PUBLIC);
         assertNotNull(cipherBytes);
         assertNotEquals(cipherBytes, plainBytes);
+        assertNotEquals(cipherBytes.length, plainBytes.length);
     }
 
     @Test
-    public void testDecrypt_Simple1() throws Exception {
-        Path keyFile = Paths.get("public.txt");
+    public void testEncrypt_withPrivateKey() throws Exception {
         RSAEncrypter encrypter = new RSAEncrypter();
         String msg = "This is a test message";
-        byte[] plainBytes = msg.getBytes(Charset.defaultCharset());
-        byte[] cipherBytes = encrypter.encrypt(plainBytes, keyFile, KeyType.PUBLIC);
-
-        byte[] decryptBytes = encrypter.decrypt(cipherBytes, Paths.get("private.txt"), KeyType.PRIVATE);
-        assertEquals(new String(plainBytes), new String(decryptBytes));
+        byte[] plainBytes = msg.getBytes();
+        byte[] cipherBytes = encrypter.encrypt(plainBytes, Paths.get("private.key"), KeyType.PRIVATE);
+        assertNotNull(cipherBytes);
+        assertNotEquals(cipherBytes, plainBytes);
+        assertNotEquals(cipherBytes.length, plainBytes.length);
     }
-    
+
+    @Test
+    public void testDecrypt_withPublicKey() throws Exception {
+        //Arrange
+        RSAEncrypter encrypter = new RSAEncrypter();
+        String msg = "This is a plain message";
+        byte[] msgBytes = msg.getBytes();
+        byte[] cipherBytes = encrypter.encrypt(msgBytes, Paths.get("private.key"), KeyType.PRIVATE);
+        //Act
+        byte[] plainBytes = encrypter.decrypt(cipherBytes, Paths.get("public.key"), KeyType.PUBLIC);
+        //Assert
+        assertNotNull(plainBytes);
+        assertEquals(msg, new String(plainBytes));
+        assertEquals(plainBytes.length, msgBytes.length);
+    }
+
+    @Test
+    public void testDecrypt_withPrivateKey() throws Exception {
+        //Arrange
+        RSAEncrypter encrypter = new RSAEncrypter();
+        String msg = "This is a plain message";
+        byte[] msgBytes = msg.getBytes();
+        byte[] cipherBytes = encrypter.encrypt(msgBytes, Paths.get("public.key"), KeyType.PUBLIC);
+        //Act
+        byte[] plainBytes = encrypter.decrypt(cipherBytes, Paths.get("private.key"), KeyType.PRIVATE);
+        //Assert
+        assertNotNull(plainBytes);
+        assertEquals(msg, new String(plainBytes));
+        assertEquals(plainBytes.length, msgBytes.length);
+    }
     @Test
     public void testSavePublicKey_Simple1() throws Exception {
         Path testFilePath = Paths.get("testFilePath.txt");
