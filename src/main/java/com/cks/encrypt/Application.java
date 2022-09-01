f73371796893a530420959af5dfe465146c04196
Colin Saldanha <colin.saldanha@gmail.com>
2022-08-31 22:23:29 -0400

    Remove unused methods in Application class

--- src/main/java/com/cks/encrypt/Application.java
+++ src/main/java/com/cks/encrypt/Application.java
@@ -7,20 +7,10 @@ package com.cks.encrypt;
 
 import com.cks.encrypt.cli.ArgsParser;
 import com.cks.encrypt.cli.Command;
-import com.cks.encrypt.encryption.AESEncrypter;
-import com.cks.encrypt.encryption.RSAEncrypter;
-import com.cks.encrypt.encryption.RSAEncrypter.KeyType;
-import com.cks.encrypt.io.FileIO;
-import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.security.GeneralSecurityException;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
-import java.util.Base64;
-import java.util.Base64.Encoder;
 import java.util.logging.*;
 
 /**
@@ -89,77 +79,5 @@ public class Application {
         System.out.printf("\t\tencrypt [aes key file] [rsa key file] <filename>\n");
         System.out.printf("\t\tdecrypt <aes key file> <rsa key file> <filename>\n");
         System.out.printf("\t\trsa [public key file] [private key file]\n");
-    }
-
-    public static void encryptFile(Path file) {
-        try {
-            Path temp = Paths.get(file.getFileName().toString() + ".secure");
-            byte[] plainBytes = FileIO.read(file);
-            AESEncrypter aes = new AESEncrypter();
-            aes.generateKey();
-            aes.generateIv();
-            byte[] cipherBytes = aes.encrypt(plainBytes);
-            FileIO.write(temp, cipherBytes);
-            aes.saveKey(Paths.get("aes.key"), Paths.get("public.txt"), KeyType.PUBLIC);
-
-        } catch (IOException x) {
-            LOGGER.severe("IO error:" + x.getMessage());
-        } catch (GeneralSecurityException x) {
-            LOGGER.severe("Security Error: " + x.getMessage());
-        }
-    }
-
-    public static void encryptFile(Path file, Path keyFile) {
-        try {
-            Path temp = Paths.get(file.getFileName().toString() + ".secure");
-            byte[] plainBytes = FileIO.read(file);
-            AESEncrypter aes = new AESEncrypter();
-            aes.loadKey(keyFile, Paths.get("private.txt"), KeyType.PRIVATE);
-            byte[] cipherBytes = aes.encrypt(plainBytes);
-            FileIO.write(temp, cipherBytes);
-
-        } catch (IOException x) {
-            LOGGER.severe("IO error:" + x.getMessage());
-        } catch (GeneralSecurityException x) {
-            LOGGER.severe("Security Error: " + x.getMessage());
-        }
-    }
-
-    public static void decryptFile(Path file) {
-        try {
-            String path = file.getFileName().toString();
-            Path decrypt = Paths.get("decrypt/" + path.substring(0, path.lastIndexOf(".")));
-            byte[] cipherBytes = FileIO.read(file);
-            AESEncrypter aes = new AESEncrypter();
-            aes.loadKey(Paths.get("aes.key"), Paths.get("private.txt"), KeyType.PRIVATE);
-            byte[] plainBytes = aes.decrypt(cipherBytes);
-            FileIO.write(decrypt, plainBytes);
-
-        } catch (IOException x) {
-            LOGGER.severe("IO error: " + x.getMessage());
-        } catch (GeneralSecurityException x) {
-            LOGGER.severe("Security error: " + x.getMessage());
-        }
-    }
-
-    public static void generateRSAKeyFiles() {
-        RSAEncrypter rsa = new RSAEncrypter();
-        try {
-            rsa.generateKeyPair();
-            Encoder encoder = Base64.getEncoder();
-            byte[] privateKeyBytes = rsa.getPrivateKey().getEncoded();
-            byte[] privateKeyBytesEncoded = encoder.encode(privateKeyBytes);
-
-            byte[] publicKeyBytes = rsa.getPublicKey().getEncoded();
-            byte[] publicKeyBytesEncoded = encoder.encode(publicKeyBytes);
-
-            FileIO.write(Paths.get("private.txt"), privateKeyBytesEncoded);
-            FileIO.write(Paths.get("public.txt"), publicKeyBytesEncoded);
-
-        } catch (GeneralSecurityException x) {
-            LOGGER.severe(x.getMessage());
-        } catch (IOException x) {
-            LOGGER.severe(x.getMessage());
-        }
     }
 }
