057548b3d5f667f6beb806c835161f0b9cde065f
Colin Saldanha <colin.saldanha@gmail.com>
2022-09-05 15:12:36 -0400

    Create KeyOnlyFlag class
    This class is used to represent flags that only have keys, and no
    values. Values field in super class is set to null and an exception is
    thrown if caller uses getValue() method

---
+++ src/main/java/com/cks/encrypt/cli/KeyOnlyFlag.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.cks.encrypt.cli;
+
+import java.util.Optional;
+
+/**
+ *
+ * @author colin.saldanha
+ */
+public class KeyOnlyFlag extends Flag<Object> {
+
+    public KeyOnlyFlag(String key) {
+        super(key, null);
+    }
+
+    @Override
+    public Optional<Object> getValue() {
+        throw new UnsupportedOperationException();
+    }
+}
