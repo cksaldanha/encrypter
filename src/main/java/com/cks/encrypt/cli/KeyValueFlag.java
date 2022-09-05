729031884da5d05b3c951e1c8a39b60e48490316
Colin Saldanha <colin.saldanha@gmail.com>
2022-09-05 15:07:28 -0400

    Create KeyValueFlag class
    This class will only be used to hold key-value pair flags. Is a simpler
    version that using the generic Flag class.

---
+++ src/main/java/com/cks/encrypt/cli/KeyValueFlag.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.cks.encrypt.cli;
+
+/**
+ *
+ * @author colin.saldanha
+ */
+public class KeyValueFlag extends Flag<String> {
+
+    public KeyValueFlag(String key, String value) {
+        super(key, value);
+    }
+}
