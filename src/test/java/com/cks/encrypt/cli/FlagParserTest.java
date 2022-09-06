0d038e7b316a7675b92b1c68771d3d0e62e25d1b
Colin Saldanha <colin.saldanha@gmail.com>
2022-09-06 11:53:13 -0400

    Update unit tests of FlagParser class
    Updated unit tests due to new sub-classes of Flag class being used,
    along with generics.

--- src/test/java/com/cks/encrypt/cli/FlagParserTest.java
+++ src/test/java/com/cks/encrypt/cli/FlagParserTest.java
@@ -5,9 +5,8 @@
  */
 package com.cks.encrypt.cli;
 
-import java.util.ArrayList;
+import java.nio.file.Paths;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 import org.junit.Test;
 
@@ -21,177 +20,166 @@ public class FlagParserTest {
 
     @Test
     public void testParseKeyValueFlags_singleKeyValueFlag() {
-        Set<Flag> actual = FlagParser.parseKeyValueFlags("aes --first=first.Values");
-        Set<Flag> expected = new HashSet<>();
-        expected.add(new Flag("first", "first.Values"));
+        Set<?> actual = FlagParser.parseKeyValueFlags("aes --first=first.Values");
+        Set<Flag<?>> expected = new HashSet<>();
+        expected.add(new KeyValueFlag("first", "first.Values"));
         assertEquals(expected, actual);
     }
 
     @Test
     public void testParseKeyValueFlags_multipleKeyValueFlags() {
-        Set<Flag> actual = FlagParser.parseKeyValueFlags("aes --first=first.Values --second=secondValue");
-        Set<Flag> expected = new HashSet<>();
-        expected.add(new Flag("first", "first.Values"));
-        expected.add(new Flag("second", "secondValue"));
+        Set<?> actual = FlagParser.parseKeyValueFlags("aes --first=first.Values --second=secondValue");
+        Set<Flag<?>> expected = new HashSet<>();
+        expected.add(new KeyValueFlag("first", "first.Values"));
+        expected.add(new KeyValueFlag("second", "secondValue"));
         assertEquals(expected, actual);
     }
 
     @Test
     public void testParseKeyValueFlags_multipleKeyValueFlagsWithSpaces() {
-        Set<Flag> actual = FlagParser.parseKeyValueFlags("aes  --first=first.Values   --second=secondValue  ");
-        Set<Flag> expected = new HashSet<>();
-        expected.add(new Flag("first", "first.Values"));
-        expected.add(new Flag("second", "secondValue"));
+        Set<?> actual = FlagParser.parseKeyValueFlags("aes  --first=first.Values   --second=secondValue  ");
+        Set<Flag<?>> expected = new HashSet<>();
+        expected.add(new KeyValueFlag("first", "first.Values"));
+        expected.add(new KeyValueFlag("second", "secondValue"));
         assertEquals(expected, actual);
     }
 
     @Test
     public void testParseKeyValueFlags_multipleKeyValueFlagsWithLineTerminator() {
-        Set<Flag> actual = FlagParser.parseKeyValueFlags("aes --first=first.Values --second=secondValue\n");
-        Set<Flag> expected = new HashSet<>();
-        expected.add(new Flag("first", "first.Values"));
-        expected.add(new Flag("second", "secondValue"));
+        Set<?> actual = FlagParser.parseKeyValueFlags("aes --first=first.Values --second=secondValue\n");
+        Set<Flag<?>> expected = new HashSet<>();
+        expected.add(new KeyValueFlag("first", "first.Values"));
+        expected.add(new KeyValueFlag("second", "secondValue"));
         assertEquals(expected, actual);
     }
 
     @Test
     public void testParseKeyOnlyFlags_singleKeyOnlyFlag() {
-        Set<Flag> actual = FlagParser.parseKeyOnlyFlags("aes --first");
-        Set<Flag> expected = new HashSet<>();
-        expected.add(new Flag("first", null));
+        Set<?> actual = FlagParser.parseKeyOnlyFlags("aes --first");
+        Set<Flag<?>> expected = new HashSet<>();
+        expected.add(new KeyOnlyFlag("first"));
         assertEquals(expected, actual);
     }
 
     @Test
     public void testParseKeyOnlyFlags_multipleKeyOnlyFlags() {
-        Set<Flag> actual = FlagParser.parseKeyOnlyFlags("aes --first --second --third");
-        Set<Flag> expected = new HashSet<>();
-        expected.add(new Flag("first", null));
-        expected.add(new Flag("second", null));
-        expected.add(new Flag("third", null));
+        Set<?> actual = FlagParser.parseKeyOnlyFlags("aes --first --second --third");
+        Set<Flag<?>> expected = new HashSet<>();
+        expected.add(new KeyOnlyFlag("first"));
+        expected.add(new KeyOnlyFlag("second"));
+        expected.add(new KeyOnlyFlag("third"));
         assertEquals(expected, actual);
     }
 
     @Test
     public void testParseKeyOnlyFlags_multipleKeyOnlyFlagsWithSpaces() {
-        Set<Flag> actual = FlagParser.parseKeyOnlyFlags("aes  --first   --second  --third  ");
-        Set<Flag> expected = new HashSet<>();
-        expected.add(new Flag("first", null));
-        expected.add(new Flag("second", null));
-        expected.add(new Flag("third", null));
+        Set<?> actual = FlagParser.parseKeyOnlyFlags("aes  --first   --second  --third  ");
+        Set<Flag<?>> expected = new HashSet<>();
+        expected.add(new KeyOnlyFlag("first"));
+        expected.add(new KeyOnlyFlag("second"));
+        expected.add(new KeyOnlyFlag("third"));
         assertEquals(expected, actual);
     }
 
     @Test
     public void testParseFlags_singleKeyValueFlag() {
-        Set<Flag> actual = FlagParser.parseFlags("aes --first=first.Values");
-        Set<Flag> expected = new HashSet<>();
-        expected.add(new Flag("first", "first.Values"));
+        Set<?> actual = FlagParser.parseFlags("aes --first=first.Values");
+        Set<Flag<?>> expected = new HashSet<>();
+        expected.add(new KeyValueFlag("first", "first.Values"));
         assertEquals(expected, actual);
     }
 
     @Test
     public void testParseFlags_multipleKeyValueFlags() {
-        Set<Flag> actual = FlagParser.parseFlags("aes --first=first.Values --second=secondValue    ");
-        Set<Flag> expected = new HashSet<>();
-        expected.add(new Flag("first", "first.Values"));
-        expected.add(new Flag("second", "secondValue"));
+        Set<?> actual = FlagParser.parseFlags("aes --first=first.Values --second=secondValue    ");
+        Set<Flag<?>> expected = new HashSet<>();
+        expected.add(new KeyValueFlag("first", "first.Values"));
+        expected.add(new KeyValueFlag("second", "secondValue"));
         assertEquals(expected, actual);
     }
 
     @Test
     public void testParseFlags_mixedFlags() {
-        Set<Flag> actual = FlagParser.parseFlags("aes --first=first.Values  --third --second=secondValue    ");
-        Set<Flag> expected = new HashSet<>();
-        expected.add(new Flag("first", "first.Values"));
-        expected.add(new Flag("second", "secondValue"));
-        expected.add(new Flag("third", null));
+        Set<?> actual = FlagParser.parseFlags("aes --first=first.Values  --third --second=secondValue    ");
+        Set<Flag<?>> expected = new HashSet<>();
+        expected.add(new KeyValueFlag("first", "first.Values"));
+        expected.add(new KeyValueFlag("second", "secondValue"));
+        expected.add(new KeyOnlyFlag("third"));
         assertEquals(expected, actual);
     }
 
     @Test
     public void testParseFlags_mixedFlagsNoLineTerminater() {
-        Set<Flag> actual = FlagParser.parseFlags("aes --first=first.Values  --third --second=secondValue --fourth");
-        Set<Flag> expected = new HashSet<>();
-        expected.add(new Flag("first", "first.Values"));
-        expected.add(new Flag("second", "secondValue"));
-        expected.add(new Flag("third", null));
-        expected.add(new Flag("fourth", null));
+        Set<?> actual = FlagParser.parseFlags("aes --first=first.Values  --third --second=secondValue --fourth");
+        Set<Flag<?>> expected = new HashSet<>();
+        expected.add(new KeyValueFlag("first", "first.Values"));
+        expected.add(new KeyValueFlag("second", "secondValue"));
+        expected.add(new KeyOnlyFlag("third"));
+        expected.add(new KeyOnlyFlag("fourth"));
         assertEquals(expected, actual);
     }
 
     @Test
     public void testParseFlags_withSingleFileName() {
-        Set<Flag> actual = FlagParser.parseFlags("aes file1");
-        Set<Flag> expected = new HashSet<>();
-        List<String> fileNames = new ArrayList<>();
-        fileNames.add("file1");
-        expected.add(new Flag("files", fileNames));
+        Set<?> actual = FlagParser.parseFlags("aes file1");
+        Set<Flag<?>> expected = new HashSet<>();
+        expected.add(new FilesFlag(Paths.get("file1")));
         assertEquals(expected, actual);
     }
 
     @Test
     public void testParseFlags_withSingleFileNameAndLineTeriminator() {
-        Set<Flag> actual = FlagParser.parseFlags("aes file1\n");
-        Set<Flag> expected = new HashSet<>();
-        List<String> fileNames = new ArrayList<>();
-        fileNames.add("file1");
-        expected.add(new Flag("files", fileNames));
+        Set<?> actual = FlagParser.parseFlags("aes file1\n");
+        Set<Flag<?>> expected = new HashSet<>();
+        FilesFlag flag = new FilesFlag(Paths.get("file1"));
+        expected.add(flag);
         assertEquals(expected, actual);
     }
 
     @Test
     public void testParseFlags_withSingleFileNameAndRandomSpaces() {
-        Set<Flag> actual = FlagParser.parseFlags("aes   file1   ");
-        Set<Flag> expected = new HashSet<>();
-        List<String> fileNames = new ArrayList<>();
-        fileNames.add("file1");
-        expected.add(new Flag("files", fileNames));
+        Set<?> actual = FlagParser.parseFlags("aes   file1   ");
+        Set<Flag<?>> expected = new HashSet<>();
+        FilesFlag flag = new FilesFlag(Paths.get("file1"));
+        expected.add(flag);
         assertEquals(expected, actual);
     }
 
     @Test
     public void testParseFlags_withMultipleFileNames() {
-        Set<Flag> actual = FlagParser.parseFlags("encrypt file1 file2");
-        Set<Flag> expected = new HashSet<>();
-        List<String> fileNames = new ArrayList<>();
-        fileNames.add("file1");
-        fileNames.add("file2");
-        expected.add(new Flag("files", fileNames));
+        Set<?> actual = FlagParser.parseFlags("encrypt file1 file2");
+        Set<Flag<?>> expected = new HashSet<>();
+        FilesFlag flag = new FilesFlag(Paths.get("file1"), Paths.get("file2"));
+        expected.add(flag);
         assertEquals(expected, actual);
     }
 
     @Test
     public void testParseFlags_withMultipleFileNamesAndLineTerminator() {
-        Set<Flag> actual = FlagParser.parseFlags("encrypt file1 file2\n");
-        Set<Flag> expected = new HashSet<>();
-        List<String> fileNames = new ArrayList<>();
-        fileNames.add("file1");
-        fileNames.add("file2");
-        expected.add(new Flag("files", fileNames));
+        Set<?> actual = FlagParser.parseFlags("encrypt file1 file2\n");
+        Set<Flag<?>> expected = new HashSet<>();
+        FilesFlag flag = new FilesFlag(Paths.get("file1"), Paths.get("file2"));
+        expected.add(flag);
         assertEquals(expected, actual);
     }
 
     @Test
     public void testParseFlags_withMultipleFileNamesAndRandomSpaces() {
-        Set<Flag> actual = FlagParser.parseFlags("encrypt   file1   file2 \n");
-        Set<Flag> expected = new HashSet<>();
-        List<String> fileNames = new ArrayList<>();
-        fileNames.add("file1");
-        fileNames.add("file2");
-        expected.add(new Flag("files", fileNames));
+        Set<?> actual = FlagParser.parseFlags("encrypt   file1   file2 \n");
+        Set<Flag<?>> expected = new HashSet<>();
+        FilesFlag flag = new FilesFlag(Paths.get("file1"), Paths.get("file2"));
+        expected.add(flag);
         assertEquals(expected, actual);
     }
 
     @Test
     public void testParseFlags_withFlagsAndFilePath() {
-        Set<Flag> actual = FlagParser.parseFlags("encrypt --mode=aes --keypath=/test/aes.key /test/file1.txt");
-        Set<Flag> expected = new HashSet<>();
-        List<String> fileNames = new ArrayList<>();
-        fileNames.add("/test/file1.txt");
-        expected.add(new Flag("mode", "aes"));
-        expected.add(new Flag("keypath", "/test/aes.key"));
-        expected.add(new Flag("files", fileNames));
+        Set<?> actual = FlagParser.parseFlags("encrypt --mode=aes --keypath=/test/aes.key /test/file1.txt");
+        Set<Flag<?>> expected = new HashSet<>();
+        expected.add(new KeyValueFlag("mode", "aes"));
+        expected.add(new KeyValueFlag("keypath", "/test/aes.key"));
+        FilesFlag filesFlag = new FilesFlag(Paths.get("/test/file1.txt"));
+        expected.add(filesFlag);
         assertEquals(expected, actual);
     }
 }
