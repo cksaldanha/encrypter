1f590d2d04ceff45fcaf7d60b9d6364b23f9da0f
Colin Saldanha <colin.saldanha@gmail.com>
2022-09-06 20:31:52 -0400

    Move command classes to their own package
    Command classes are placed into com.cks.encrypt.cli.command package.
    This is because they are more closely related, plus this package can be
    extended with more commands in the future.

--- src/main/java/com/cks/encrypt/cli/command/DecryptCommand.java
+++ src/main/java/com/cks/encrypt/cli/command/DecryptCommand.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.cks.encrypt.cli;
+package com.cks.encrypt.cli.command;
 
 import com.cks.encrypt.cli.flag.Flag;
 import com.cks.encrypt.cli.flag.KeyValueFlag;
